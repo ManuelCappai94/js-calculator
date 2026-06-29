08c78dee9b5e50b610a99bf03b290274d92056ef
ManuelCappai94 <manuelcappai@outlook.it>
2026-06-29 16:55:04 +0200

    added event delegation

--- src/script/script.js
+++ src/script/script.js
@@ -1,23 +1,32 @@
 const calcButtons = document.querySelector(".calcButtons")
 
+let currentInput = ""
+
 
 function initCalcDigits(){
     calcButtons.addEventListener("click", (e)=>{
       const digit =  e.target.closest("[data-number]")
       const operator = e.target.closest("[data-operator]")
+      const actionBtns = e.target.closest("[data-action]")
 
     //   if(!digit)return
     //   console.log(digit.dataset.number, operator.dataset.operator)
     if(digit){
-        console.log(digit.dataset.number)
+        // console.log(digit.dataset.number)
+        currentInput += digit.dataset.number
+        console.log(currentInput)
         return
     }
     if(operator){
         console.log(operator.dataset.operator)
         return
     }
+    if(actionBtns){
+        console.log(actionBtns.dataset.action)
+        return
+    }
     })
 }
-console.log(calcButtons)
+
 
 initCalcDigits()
